720dbc922624398f1636274ca27f63ecbdb5ee9c
Documenter.jl <documenter@juliadocs.github.io>
2022-05-20 09:53:34 +0000

    build based on dac2d9a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OptControl","category":"page"},{"location":"#OptControl","page":"Home","title":"OptControl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OptControl.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OptControl]","category":"page"}]
+}
