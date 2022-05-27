26fc28de291501d79a2c76821a1a324d748e82df
yjy <522432938@qq.com>
2022-05-27 10:03:26 +0800

    fix deploydoc

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,14 @@
 using OptControl
 using Documenter
 
-DocMeta.setdocmeta!(OptControl, :DocTestSetup, :(using OptControl); recursive=true)
-
 makedocs(;
     modules=[OptControl],
     authors="yjy <522432938@qq.com> and contributors",
-    repo="https://github.com/jake484/OptControl.jl/blob/{commit}{path}#{line}",
     sitename="OptControl.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://jake484.github.io/OptControl.jl",
-        # assets="assets/logo.svg",
+        # assets=["assets/logo.svg"]
     ),
     pages=[
         "Home" => "index.md",
@@ -21,10 +18,9 @@ makedocs(;
             "basics/DEsolution.md",
             "basics/discretization.md"
         ]
-    ],
+    ]
 )
 
-deploydocs(;
-    repo="github.com/jake484/OptControl.jl",
-    push_preview=true,
-)
+deploydocs(
+    repo="github.com/jake484/OptControl.jl";
+    push_preview=true)
